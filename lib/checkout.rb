eb0a6656f2b8a0735a06d3db85b8e9d0ca3d2eb4
Jamie Brown <jamiebrown201@hotmail.com>
2016-05-16 16:07:41 +0100

    current_order has implementation with scan adding

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -2,18 +2,20 @@ require 'bigdecimal'
 require_relative 'basket'
 
 class Checkout
+  attr_reader :product_list
+  attr_accessor :current_order
 
   def initialize(product_list)
     @product_list = product_list
+    @current_order = Hash.new(0)
   end
 
   def scan(item_code)
     fail "Cannot find a product with #{item_code} in the product list" unless item_in_product_list?(item_code)
-    order[item_code] += 1
+    current_order[item_code] += 1
   end
 
   private
-  attr_reader :product_list
 
   def item_in_product_list?(item_code)
     product_list.map{ |product| product.code }.include?(item_code)
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -13,5 +13,9 @@ describe Checkout do
     it 'is expected to not raise an error if given a code that is valid' do
       expect{ (checkout.scan('001')) }.to_not raise_error 'Cannot find a product with 001 in the product list'
     end
+    it 'adds the item to the current_order hash' do
+      checkout.scan('001')
+      expect(checkout.current_order).to include({"001" => 1})
+    end
   end
 end
