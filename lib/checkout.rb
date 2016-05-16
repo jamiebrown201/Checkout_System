2acb5ece0d799b808d7c579a21fa14223233ed46
Jamie Brown <jamiebrown201@hotmail.com>
2016-05-16 15:36:19 +0100

    test pasing for not raising error

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -2,11 +2,21 @@ require 'bigdecimal'
 require_relative 'basket'
 
 class Checkout
-  def initialize(inventory)
-    @inventory = inventory
+
+  def initialize(product_list)
+    @product_list = product_list
   end
 
   def scan(item_code)
-    fail "Cannot find a product with #{item_code} in the product list"
+    fail "Cannot find a product with #{item_code} in the product list" unless item_in_product_list?(item_code)
+    order[item_code] += 1
   end
+
+  private
+  attr_reader :product_list
+
+  def item_in_product_list?(item_code)
+    product_list.map{ |product| product.code }.include?(item_code)
+  end
+
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -10,5 +10,8 @@ describe Checkout do
     it 'is expected to raise an error if given a code that is not valid' do
       expect{ (checkout.scan('011')) }.to raise_error 'Cannot find a product with 011 in the product list'
     end
+    it 'is expected to not raise an error if given a code that is valid' do
+      expect{ (checkout.scan('001')) }.to_not raise_error 'Cannot find a product with 001 in the product list'
+    end
   end
 end
