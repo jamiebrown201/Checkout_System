2eea57889a69cf209b53b941cc131526bd09dbe7
Jamie Brown <jamiebrown201@hotmail.com>
2016-05-16 13:52:41 +0100

    scan fails with invalid number

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -10,8 +10,5 @@ describe Checkout do
     it 'is expected to raise an error if given a code that is not valid' do
       expect{ (checkout.scan('011')) }.to raise_error 'Cannot find a product with 011 in the product list'
     end
-    it 'is expected to raise an error if given a code that is not valid' do
-      expect{ (checkout.scan('011')) }.to raise_error 'Cannot find a product with 011 in the product list'
-    end
   end
 end
