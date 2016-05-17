dcb986fa025154b89e8fdc6eb163247329da3cfa
Jamie Brown <jamiebrown201@hotmail.com>
2016-05-17 12:18:19 +0100

    percentage ppromotion

--- lib/promotions/percentage.rb
+++ lib/promotions/percentage.rb
@@ -4,4 +4,19 @@ class Percentage
     @minimum_price = minimum_price
   end
 
+  def apply(current_total)
+    is_order_eligible?(current_total) ? apply_promotion(current_total) : 0
+  end
+
+  private
+
+  attr_reader :minimum_price, :percentage
+
+  def is_order_eligible?(current_total)
+    current_total > minimum_price
+  end
+
+  def apply_promotion(current_total)
+    current_total - (current_total / percentage)
+  end
 end
