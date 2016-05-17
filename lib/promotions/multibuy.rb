df37a477104db070d65ab2d2ed652c3868788c72
Jamie Brown <jamiebrown201@hotmail.com>
2016-05-17 14:37:56 +0100

    Multi buy promotion implmentation

--- lib/promotions/multibuy.rb
+++ lib/promotions/multibuy.rb
@@ -5,4 +5,19 @@ class MultiBuy
     @minimum_items = minimum_items
   end
 
+  def apply(current_total, current_order)
+    is_order_eligible?(current_order) ? apply_promotion(current_order) : 0
+  end
+
+  private
+
+  attr_reader :minimum_items, :discount, :product
+
+  def apply_promotion(current_order)
+    current_order[product] * discount
+  end
+
+  def is_order_eligible?(current_order)
+    current_order[product] >= minimum_items
+  end
 end
