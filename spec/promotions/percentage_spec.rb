a6374140b9181d72dd512d0e22de40819da9d184
Jamie Brown <jamiebrown201@hotmail.com>
2016-05-24 11:42:44 +0100

    percentage refactor

--- spec/promotions/percentage_spec.rb
+++ spec/promotions/percentage_spec.rb
@@ -1,15 +1,14 @@
-require_relative '../../lib/promotions/percentage'
+require_relative '../../lib/promotions/percentage.rb'
 
 describe Percentage do
-  subject(:percentage) { described_class.new(10, 50) }
-
-  describe '#apply' do
-    it 'applies a discount if order total is over a minimum price' do
-      expect(percentage.apply(60, 100)).to eq 6
+  subject(:percentage) { described_class.new({minimum_spend: BigDecimal("60"),
+                                              percentage: 10})}
+  describe '#apply'  do
+    it 'returns the amount discounted when the order is valid' do
+      expect(percentage.apply(60, { "TEST" => 4 })).to eq(BigDecimal("6"))
     end
-
-    it 'does not apply the discount if the order is below the minimum price' do
-      expect(percentage.apply(40, 100)).to eq 0
+    it 'returns no discount when the order is invalid' do
+      expect(percentage.apply(40, { "TEST" => 4 })).to eq(0)
     end
   end
 end
