24d04a37664182234841c08935765d3aa77ef73f
Jamie Brown <jamiebrown201@hotmail.com>
2016-05-24 11:54:35 +0100

    integration spec refactor

--- spec/features/integration_spec.rb
+++ spec/features/integration_spec.rb
@@ -1,41 +1,46 @@
 require_relative '../../lib/checkout'
 require_relative '../../lib/item_factory'
+require_relative '../../lib/basket'
 require_relative '../../lib/promotions/percentage'
 require_relative '../../lib/promotions/multibuy'
-describe 'Integration spec' do
-  let(:products) do
+
+describe 'integration spec' do
+  let(:inventory) do
     [
-      Item.new('001','Travel Card Holder', BigDecimal("9.25")),
-      Item.new('002','Personalised cufflinks', BigDecimal("45.00")),
-      Item.new('003','Kids T-shirt', BigDecimal("19.95"))
+      ItemFactory.build(ItemConfig.travel_card_holder),
+      ItemFactory.build(ItemConfig.personalised_cufflinks),
+      ItemFactory.build(ItemConfig.kids_tee)
     ]
   end
-
-  let(:ten_percent_discount) { Percentage.new(10, BigDecimal("60.00")) }
-  let(:travel_card_discount) { MultiBuy.new("001", 2, BigDecimal("0.75")) }
+  let(:ten_percent_discount) { Percentage.new({minimum_spend: BigDecimal("60"),
+                                               percentage: 10}) }
+  let(:travel_card_discount) { Multibuy.new({item_code: "001",
+                                             minimum_quantity: 2,
+                                             discount: BigDecimal("0.75")}) }
   let(:promotions) { [travel_card_discount, ten_percent_discount] }
-  subject(:checkout) { Checkout.new(products, Basket, promotions) }
+  subject(:checkout) { Checkout.new({inventory: inventory,
+                                     basket_klass: Basket,
+                                     promotions: promotions}) }
 
-  it 'applies a 10% discount when order is over £60' do
-    checkout.scan '001'
-    checkout.scan '002'
-    checkout.scan '003'
-    expect(checkout.total).to eq '£66.78'
+  it 'applies a 10% discount when the order is over £60' do
+    checkout.scan("001")
+    checkout.scan("002")
+    checkout.scan("003")
+    expect(checkout.total).to eq('£66.78')
   end
 
   it 'applies a multibuy discount' do
-    checkout.scan '001'
-    checkout.scan '003'
-    checkout.scan '001'
-    expect(checkout.total).to eq '£36.95'
+    checkout.scan('001')
+    checkout.scan('003')
+    checkout.scan('001')
+    expect(checkout.total).to eq('£36.95')
   end
 
   it 'applies both discounts' do
-    checkout.scan '001'
-    checkout.scan '002'
-    checkout.scan '001'
-    checkout.scan '003'
-    expect(checkout.total).to eq '£73.76'
+    checkout.scan('001')
+    checkout.scan('002')
+    checkout.scan('001')
+    checkout.scan('003')
+    expect(checkout.total).to eq('£73.76')
   end
-
 end
